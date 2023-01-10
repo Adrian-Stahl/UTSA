8a59bafc99aabc55478c96b0e1623695507562c0
adrian.stahl <118782485+Adrian-Stahl@users.noreply.github.com>
2023-01-09 20:59:32 -0600

    class

--- 3/03-Stu_HobbyBook-Dictionaries/Unsolved/HobbyBook_Unsolved.py
+++ 3/03-Stu_HobbyBook-Dictionaries/Unsolved/HobbyBook_Unsolved.py
@@ -1,1 +1,11 @@
-# @TODO: Your code here
+# TODO: Your code here
+#Create a dictionary to store the following information:
+
+
+#Your name
+#Your age
+#A list of a few of your hobbies
+#A dictionary that includes a few days and the time you typically wake up on those days
+
+
+#Print out your name, how many hobbies you have, and a time you typically wake up during the week.
